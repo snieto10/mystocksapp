2da20779354b3d0ea1eef80fd0cd1dec504734ec
Sergio Nieto <snieto10@gmail.com>
2022-10-04 21:34:03 -0400

    add stock button added

--- src/components/addStock.jsx
+++ src/components/addStock.jsx
@@ -1,14 +1,58 @@
 import React from "react";
 import "./addStock.css";
 
-const AddStock = () => {
-  return (
-    <div className="blockcenter">
-      <div className="block1">
-        <h1 className="title">My Stock List</h1>
+class AddStock extends React.Component {
+  state = {};
+  render() {
+    return (
+      <div className="fullpage">
+        <div className="addstock">
+          <div className="box">
+            <form>
+              <input
+                className="therealinput"
+                type="text"
+                id="stockSticker"
+                placeholder="Sticker"
+              ></input>
+              <br />
+              <input
+                className="therealinput"
+                type="text"
+                id="CompanyName"
+                placeholder="Company Name"
+              ></input>
+              <br />
+              <input
+                className="therealinput"
+                type="text"
+                id="qty"
+                placeholder="Qty"
+              ></input>
+              <br />
+              <input
+                className="therealinput"
+                type="text"
+                id="Price"
+                placeholder="Price"
+              ></input>
+              <br />
+              <input
+                className="therealinput"
+                type="text"
+                id="value"
+                placeholder="Value"
+              ></input>
+              <br />
+              <div className="centel">
+                <button className="btn-input">Add Stock</button>
+              </div>
+            </form>
+          </div>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default AddStock;
